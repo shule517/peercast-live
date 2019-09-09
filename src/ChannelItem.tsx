9121e9f4be54e92dcac04b206152797b7d59cf71
shule517 <shule517@gmail.com>
2019-09-09 23:02:15 +0900

    styled-componentsを導入

--- src/ChannelItem.tsx
+++ src/ChannelItem.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import Channel from './Channel';
 
 declare var flvjs: any;
@@ -7,14 +8,22 @@ type Props = {
     channel: Channel,
 }
 
-const ChannelItem = (props: Props) => {
+const StyledButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;`
+
+  const ChannelItem = (props: Props) => {
     const {
         channel,
     } = props;
 
     return (
         <div>
-            <button onClick={() => {
+            <StyledButton onClick={() => {
                 var videoElement = document.getElementById('videoElement');
                 const url = `http://ec2-18-222-202-2.us-east-2.compute.amazonaws.com:7144/stream/${channel.streamId}.flv?auth=JkVYNUxQTmVaTUEwWSdlMDouSWhDQnE7c1lZKCFyeXVY`;
                 var flvPlayer = flvjs.createPlayer({
@@ -25,7 +34,7 @@ const ChannelItem = (props: Props) => {
                 flvPlayer.attachMediaElement(videoElement);
                 flvPlayer.load();
                 flvPlayer.play();
-            }}>再生</button>
+            }}>再生</StyledButton>
             {`【${channel.name}】 ${channel.genre} - ${channel.details.replace(/ - .*/, '')}`}
         </div>
     );
